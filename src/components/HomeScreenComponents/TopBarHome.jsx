cd7b8a28fc483069277506d5894581c48924fcf5
irinosochka <irinovoselska@gmail.com>
2022-11-19 18:29:05 +0100

    changed topBar to component

---
+++ src/components/HomeScreenComponents/TopBarHome.jsx
@@ -0,0 +1,37 @@
+import {
+    StyleSheet, Text, View, TouchableOpacity,
+} from 'react-native';
+import React, {useContext} from 'react';
+import {FormStyles} from "../../styles/Styles";
+import {UserDataContext} from "../../context/UserDataContext";
+import Icon from "react-native-vector-icons/Feather";
+
+const styles = StyleSheet.create({
+    homeWrapper: {
+        flex: 1,
+        paddingTop: 80,
+    },
+    topWrapper: {
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+});
+
+function TopBarHome({ navigation }) {
+
+    const { userData, setUserData } = useContext(UserDataContext)
+
+    return (
+            <View style={styles.homeWrapper}>
+                <View style={styles.topWrapper}>
+                    <Text style={FormStyles.title}>Hello, {userData.name} &#128075; </Text>
+                    <TouchableOpacity>
+                        <Icon name="bell" size={23} color={'#9B9B9B'} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+    );
+}
+
+export default TopBarHome;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,41 +1,21 @@
 import {
-    StyleSheet, Text, View, TouchableOpacity,
+    StyleSheet, View,
 } from 'react-native';
-import React, {useContext} from 'react';
-import {colors, FormStyles} from "../styles/Styles";
-import {UserDataContext} from "../context/UserDataContext";
-import Icon from "react-native-vector-icons/Feather";
+import React from 'react';
+import TopBarHome from "../components/HomeScreenComponents/TopBarHome";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffffff',
     },
-    homeWrapper: {
-        flex: 1,
-        paddingTop: 80,
-    },
-    topWrapper: {
-        paddingHorizontal: 20,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    }
 });
 
 function HomeScreen({ navigation }) {
 
-    const { userData, setUserData } = useContext(UserDataContext)
-
     return (
         <View style={styles.container}>
-            <View style={styles.homeWrapper}>
-                <View style={styles.topWrapper}>
-                    <Text style={FormStyles.title}>Hello, {userData.name} &#128075; </Text>
-                    <TouchableOpacity>
-                        <Icon name="bell" size={23} color={'#9B9B9B'} />
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <TopBarHome/>
         </View>
     );
 }
